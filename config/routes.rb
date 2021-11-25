57c2a2dfaef8bcfabc1ef33d0f51fd81a8388818
paruharu <oha.ru.sunny0861@gmail.com>
2021-11-25 13:47:16 +0000

    1125database

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  
+  namespace :admin do
+    resources :items
+    resources :genres
+    resources :customers
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+    root to: 'homes#top'
+    
+  end
+
+  scope module: :public do
+    resources :customers
+    resources :items, only: [:index, :show]
+    resources :cart_items
+    resources :orders
+    resources :addresses
+    root to: 'homes#top'
+    get "/about" => "homes#about"
+  end
+
+  
 end
