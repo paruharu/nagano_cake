c7c3361a72b04d8869530bb8befd1fae9adadcc5
paruharu <oha.ru.sunny0861@gmail.com>
2021-12-30 12:14:26 +0000

    1230adddefult

--- db/schema.rb
+++ db/schema.rb
@@ -83,7 +83,7 @@ ActiveRecord::Schema.define(version: 2021_12_04_010753) do
     t.integer "order_id"
     t.integer "price"
     t.integer "amount"
-    t.integer "making_status"
+    t.integer "making_status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
