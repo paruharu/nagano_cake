942a667d8a020f9d9388d048b3735f7b807baef9
paruharu <oha.ru.sunny0861@gmail.com>
2022-01-05 13:20:48 +0000

    0105correctview

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -19,7 +19,7 @@ class Admin::GenresController < ApplicationController
   def update
     @genre = Genre.find(params[:id])
      if @genre.update(genre_params)
-        flash[:notice] = "更新しました"
+        flash[:notice] = "更新しました。"
         redirect_to admin_genres_path
      else
         render :edit
